dc031a075d4ad50d5ac223e130c4c3cf13a89234
RONNEL NAVARRO <uptimisedcorporation2022@gmail.com>
2023-04-01 00:20:16 +0800

    ito na

--- system/include/header.php
+++ system/include/header.php
@@ -2,7 +2,7 @@
 
   session_start();
   include './function.php';
-    $connect = mysqli_connect('localhost', 'u708090748_uptimised', '@User2022', 'u708090748_uptimisedph'); 
+    // $connect = mysqli_connect('localhost', 'u708090748_uptimised', '@User2022', 'u708090748_uptimisedph'); 
     // $connect = mysqli_connect('localhost', 'u817058626_uptimisedph', 'Uptimised2022', 'u817058626_uptimisedph'); 
     // $connect = mysqli_connect('localhost', 'root', '', 'uptimisedph');
 
